0481e6896592d5868fff99ea4354d6badd41d425
Hwi-sang Cho <hwisang@sas.upenn.edu>
2023-05-02 20:23:53 -0400

    main function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user for the start and end pages.
+        System.out.print("Enter the start page: ");
+        String startTitle = scanner.nextLine();
+        System.out.print("Enter the end page: ");
+        String endTitle = scanner.nextLine();
+
+        // Create the start and end page objects.
+        WikipediaPage start = new WikipediaPage(startTitle);
+        WikipediaPage end = new WikipediaPage(endTitle);
+
+        // Create the game board object.
+        WikipediaGameBoard gameBoard = new WikipediaGameBoard(start, end);
+
+        // Keep playing until the start and end pages are related.
+        while (!gameBoard.isRelated()) {
+
+        }
+
+        // Print the path.
+
+        scanner.close();
     }
+
 }
